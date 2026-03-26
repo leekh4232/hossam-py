61d5b17264d2cb4bfbd7d679c07c2e51ceab5e5e
이광호 <leekh4232@gmail.com>
2026-03-26 20:22:59 +0900

    auto commit 2026-03-26 20:22:56

--- hossam/__init__.py
+++ hossam/__init__.py
@@ -1,7 +1,9 @@
+import os
+IMPORT_VIA = os.environ.get("_HOSSAM_IMPORT_VIA", "direct")
+
 import importlib.metadata
 import multiprocessing as mp
 import requests
-import os
 from pathlib import Path
 
 # submodules
@@ -50,10 +52,12 @@ __all__ = [
 
 
 def check_pypi_latest(package_name: str):
-    # 설치된 버전
-    installed = importlib.metadata.version(package_name)
+    installed = None
+    latest = None
 
     try:
+        # 설치된 버전
+        installed = importlib.metadata.version(package_name)
         # PyPI 최신 버전
         url = f"https://pypi.org/pypi/{package_name}/json"
         resp = requests.get(url, timeout=5)
@@ -66,32 +70,33 @@ def check_pypi_latest(package_name: str):
         "package": package_name,
         "installed": installed,
         "latest": latest,
-        "outdated": installed < latest, # type: ignore
+        "outdated": installed is not None and latest is not None and installed != latest
     }
 
 def _init():
 
-    # 안내 메시지 (블릿 리스트)
-    messages = [
-        "📦 아이티윌 이광호 강사가 제작한 라이브러리를 사용중입니다.",
-        "📚 자세한 사용 방법은 https://py.hossam.kr 을 참고하세요.",
-        "📧 Email: leekh4232@gmail.com",
-        "🎬 Youtube: https://www.youtube.com/@hossam-codingclub",
-        "📝 Blog: https://blog.hossam.kr/",
-        f"🔖 Version: {__version__}",
-    ]
+    if IMPORT_VIA == "direct":
+        # 안내 메시지 (블릿 리스트)
+        messages = [
+            "📦 아이티윌 이광호 강사가 제작한 라이브러리를 사용중입니다.",
+            "📚 자세한 사용 방법은 https://py.hossam.kr 을 참고하세요.",
+            "📧 Email: leekh4232@gmail.com",
+            "🎬 Youtube: https://www.youtube.com/@hossam-codingclub",
+            "📝 Blog: https://blog.hossam.kr/",
+            f"🔖 Version: {__version__}",
+        ]
 
-    for msg in messages:
-        print(f"{msg}")
+        for msg in messages:
+            print(f"{msg}")
 
-    version_info = check_pypi_latest("hossam")
+        version_info = check_pypi_latest("hossam")
 
-    if __version__ != "develop" and version_info["outdated"]:
-        print(
-            f"\n⚠️  'hossam' 패키지의 최신 버전이 출시되었습니다! (설치된 버전: {version_info['installed']}, 최신 버전: {version_info['latest']})"
-        )
-        print("   최신 버전으로 업데이트하려면 다음 명령어를 실행하세요:")
-        print("   pip install --upgrade hossam\n")
+        if __version__ != "develop" and version_info["outdated"]:
+            print(
+                f"\n⚠️  'hossam' 패키지의 최신 버전이 출시되었습니다! (설치된 버전: {version_info['installed']}, 최신 버전: {version_info['latest']})"
+            )
+            print("   최신 버전으로 업데이트하려면 다음 명령어를 실행하세요:")
+            print("   pip install --upgrade hossam\n")
 
     # 각 열의 넓이 제한 없음
     pd.set_option("display.max_colwidth", None)
